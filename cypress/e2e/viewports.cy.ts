02a84b2f290cab5e8469f709d29a8988eeedcf7a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-16 13:37:02 -0500

    use test object

--- cypress/e2e/viewports.cy.ts
+++ cypress/e2e/viewports.cy.ts
@@ -6,32 +6,29 @@ beforeEach(() => {
 })
 
 describe('Viewports', () => {
-  const resolutions = [
-    [1280, 1024], // [w, h]
-    [700, 500],
-    [300, 500],
-  ]
+  // type each number in the arguments arrays [w, h]
+  const resolutions: TestCaseObject2<number, number> = {
+    desktop: [1280, 1024], // [w, h]
+    tablet: [700, 500],
+    mobile: [300, 500],
+  }
 
-  it.each(resolutions)(
-    'works in resolution %d x %d',
-    // @ts-ignore
-    (w: number, h: number) => {
-      cy.viewport(w, h)
-      cy.visit('/')
-      cy.get('.new-todo')
-        .type('one{enter}')
-        .type('two{enter}')
-        .type('three{enter}')
-      cy.get('li.todo').should('have.length', 3)
-      cy.contains('.todo-count', '3')
-      cy.contains('li.todo', 'two').find('.toggle').click()
-      cy.contains('li.todo', 'two').should('have.class', 'completed')
-      cy.contains('.todo-count', '2')
-      cy.intercept('DELETE', '/todos/*').as('delete')
-      cy.contains('button', 'Clear completed').click()
-      cy.get('li.todo').should('have.length', 2)
-      cy.wait('@delete').its('response.statusCode').should('equal', 200)
-      cy.request('GET', '/todos').its('body').should('have.length', 2)
-    },
-  )
+  it.each(resolutions)((w, h) => {
+    cy.viewport(w, h)
+    cy.visit('/')
+    cy.get('.new-todo')
+      .type('one{enter}')
+      .type('two{enter}')
+      .type('three{enter}')
+    cy.get('li.todo').should('have.length', 3)
+    cy.contains('.todo-count', '3')
+    cy.contains('li.todo', 'two').find('.toggle').click()
+    cy.contains('li.todo', 'two').should('have.class', 'completed')
+    cy.contains('.todo-count', '2')
+    cy.intercept('DELETE', '/todos/*').as('delete')
+    cy.contains('button', 'Clear completed').click()
+    cy.get('li.todo').should('have.length', 2)
+    cy.wait('@delete').its('response.statusCode').should('equal', 200)
+    cy.request('GET', '/todos').its('body').should('have.length', 2)
+  })
 })
