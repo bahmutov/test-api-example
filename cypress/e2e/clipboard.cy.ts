9acb18cd9bc6197cb9ed061480a5c68ad7a019eb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-20 15:30:59 -0500

    playing with tests

--- cypress/e2e/clipboard.cy.ts
+++ cypress/e2e/clipboard.cy.ts
@@ -1,17 +1,9 @@
+import 'cypress-real-events'
 import { todos } from '../fixtures/three.json'
 
 it('copies the todos to clipboard', () => {
   cy.request('POST', '/reset', { todos })
   cy.visit('/')
   cy.get('li.todo').should('have.length', todos.length)
-  cy.window()
-    .its('navigator.clipboard')
-    .then((clipboard) => {
-      cy.stub(clipboard, 'writeText').as('writeText')
-    })
-  cy.get('[title="Copy todos to clipboard"]').click()
-  cy.get('@writeText').should(
-    'have.been.calledOnceWith',
-    Cypress.sinon.match.string,
-  )
+  cy.get('[title="Copy todos to clipboard"]').realClick()
 })
