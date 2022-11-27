07e45588b1a6092d6dd6e89e36b445f7f2eb7865
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-27 13:14:37 -0500

    cypress-should-really

--- cypress/e2e/sort.cy.ts
+++ cypress/e2e/sort.cy.ts
@@ -1,3 +1,5 @@
+import { map, tap, really } from 'cypress-should-really'
+
 // the initial Todo items are NOT sorted alphabetically
 const todos = [
   {
@@ -10,12 +12,6 @@ const todos = [
   },
 ]
 
-// utility function that extracts the text contents
-// of multiple elements in the given jQuery object
-function getTexts($el: JQuery) {
-  return Cypress._.map($el, 'innerText')
-}
-
 it('sorts items', () => {
   // reset the backend data by making an API call
   // https://on.cypress.io/request
@@ -25,4 +21,14 @@ it('sorts items', () => {
   cy.visit('/')
   // confirm the application shows the loaded todo items
   cy.get('li.todo').should('have.length', todos.length)
+  cy.get('[data-cy="sort-slowly"]').click()
+  cy.get('li.todo label').should(
+    really(
+      tap(console.log),
+      map('innerText'),
+      tap(console.log),
+      'deep.equal',
+      ['apple', 'ball'],
+    ),
+  )
 })
