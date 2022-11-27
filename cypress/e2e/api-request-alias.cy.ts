0b3d7954c8f065b83cd96deb0ecb6c84b6faf538
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-27 14:18:57 -0500

    alias keep true

--- cypress/e2e/api-request-alias.cy.ts
+++ cypress/e2e/api-request-alias.cy.ts
@@ -1,14 +1,26 @@
-it('makes API requests using the previous value alias', () => {
+import 'cypress-aliases'
+
+before(() => {
   // reset all current todo items
   // by requesting "POST /reset" with the { todos: [] } object
   // https://on.cypres.io/request
   //
   // add a new todo item by making "POST /todos"
   // with the title "test todo"
-  //
+  cy.request('POST', '/reset', { todos: [] })
+  cy.request('POST', '/todos', { title: 'test todo' })
+    .its('body.id')
+    .as('todoId', { keep: true })
+})
+
+it('created the correct todo', () => {
   // request the Todo item
   // confirm the response body includes the correct title
-  //
+  cy.request('/todos/@todoId')
+})
+
+it('completes the created todo', () => {
   // update the created todo item, set the "completed: true"
   // using the "PATCH /todos/:id" request
+  cy.request('PATCH', '/todos/@todoId', { completed: true })
 })
