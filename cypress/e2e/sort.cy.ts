1d73a2ca45553a7bc5470e13fa32eb6b7731594b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-27 12:52:05 -0500

    lesson i2

--- cypress/e2e/sort.cy.ts
+++ cypress/e2e/sort.cy.ts
@@ -1,5 +1,12 @@
+import { recurse } from 'cypress-recurse'
+
+type Todo = {
+  id: string
+  title: string
+}
+
 // the initial Todo items are NOT sorted alphabetically
-const todos = [
+const todos: Todo[] = [
   {
     id: '1',
     title: 'ball',
@@ -25,4 +32,23 @@ it('sorts items', () => {
   cy.visit('/')
   // confirm the application shows the loaded todo items
   cy.get('li.todo').should('have.length', todos.length)
+  cy.get('[data-cy="sort-slowly"]').click()
+  recurse(
+    () => cy.request('/todos').its('body'),
+    (todos) =>
+      expect(Cypress._.map(todos, 'title')).to.deep.equal([
+        'apple',
+        'ball',
+      ]),
+    {
+      log: 'Sorted!',
+      delay: 500,
+      timeout: 10_000,
+    },
+  ).then((todos) => {
+    todos.forEach((todo: Todo, k: number) => {
+      // use a single jQuery index to pick the label at position K
+      cy.contains(`li.todo label:eq(${k})`, todo.title)
+    })
+  })
 })
