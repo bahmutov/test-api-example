545e0d1911fc81022e3e4a7e53de157de349c248
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-28 09:47:00 -0500

    bonuses

--- cypress/e2e/reset.cy.ts
+++ cypress/e2e/reset.cy.ts
@@ -1,33 +1,51 @@
 // https://github.com/bahmutov/cypress-data-session
 import 'cypress-data-session'
+import 'cypress-if'
 
 beforeEach(() => {
-  // clear all todos using cy.dataSession command
-  // -> the session name could be any string
-  // -> the setup command should use cy.request
-  //    to clear all todos on the server
-  // .  cy.request('POST', '/reset', { todos: [] })
-  // -> the validate command should get all
-  //    todos and return true if there are none
-  // Tip: use the options syntax for simplicity
-  // cy.dataSession({ name: ..., ... })
-  cy.dataSession({
-    name: 'reset all todos',
-    setup() {
-      cy.log('in setup')
-      return cy.request('POST', '/reset', { todos: [] })
-    },
-    validate() {
-      cy.log('in validate')
-      return cy
-        .request('GET', '/todos')
-        .then((r) => r.body.length === 0)
-    },
-    recreate() {
-      cy.log('in recreate')
-    },
-  })
+  cy.request('GET', '/todos')
+    .its('body.length')
+    .if('not.equal', 0)
+    .request('POST', '/reset', { todos: [] })
 })
+
+// beforeEach(() => {
+//   cy.request('GET', '/todos')
+//     .then((r) => r.body.length === 0)
+//     .then((empty) => {
+//       if (!empty) {
+//         cy.request('POST', '/reset', { todos: [] })
+//       }
+//     })
+// })
+
+// beforeEach(() => {
+//   // clear all todos using cy.dataSession command
+//   // -> the session name could be any string
+//   // -> the setup command should use cy.request
+//   //    to clear all todos on the server
+//   // .  cy.request('POST', '/reset', { todos: [] })
+//   // -> the validate command should get all
+//   //    todos and return true if there are none
+//   // Tip: use the options syntax for simplicity
+//   // cy.dataSession({ name: ..., ... })
+//   cy.dataSession({
+//     name: 'reset all todos',
+//     setup() {
+//       cy.log('in setup')
+//       return cy.request('POST', '/reset', { todos: [] })
+//     },
+//     validate() {
+//       cy.log('in validate')
+//       return cy
+//         .request('GET', '/todos')
+//         .then((r) => r.body.length === 0)
+//     },
+//     recreate() {
+//       cy.log('in recreate')
+//     },
+//   })
+// })
 
 it('starts with zero todos', () => {
   cy.visit('/')
