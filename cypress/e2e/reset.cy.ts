451a12b9c1aad0871923fa3777bc9538ff6c8513
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-28 13:59:49 -0500

    solved bonus

--- cypress/e2e/reset.cy.ts
+++ cypress/e2e/reset.cy.ts
@@ -1,34 +1,59 @@
-it('resets the todos', () => {
-  // make an API call to "POST /reset"
-  // sending an object with "todos: []" property
-  // https://github.com/bahmutov/json-server-reset
-  //
-  // confirm the data has been cleared
-  // by fetching it using "GET /todos" and
-  // confirming the body is an empty list
+import 'cypress-data-session'
+
+beforeEach(() => {
+  // create a Todo item with text "Write more code"
+  // the data session validates the item
+  // and saves the item's "id" under the alias "todo"
+  cy.dataSession({
+    name: 'todo',
+    setup() {
+      cy.request('POST', '/todos', {
+        title: 'Write more code',
+      }).its('body.id')
+    },
+    validate(id) {
+      return cy
+        .request({
+          method: 'GET',
+          url: '/todos/' + id,
+          failOnStatusCode: false,
+        })
+        .its('isOkStatusCode')
+    },
+  })
 })
 
-it('resets the todos to a fixture', () => {
-  // load the fixture file "three.json"
-  // https://on.cypress.io/fixture
-  // The loaded data is an object with "todos: [...]"
-  //
-  // make an API call to reset the backend data
-  // "POST /reset data"
-  //
-  // confirm the backend now has 3 todos
-  // (or as many as the data.todos list has)
-  // by fetching it from the backend
-  //
-  // visit the application page
-  // https://on.cypress.io/visit
-  //
-  // confirm the number of shown todos is equal
-  // to the number of todos loaded from the fixture file
-  //
-  // verify the todos loaded from the fixture file
-  // are correctly shown on the web page
-  // - the order is correct
-  // - the right title text is there
-  // - the completed todos are marked correctly
+beforeEach(function () {
+  // call cy.dataSession to complete the item
+  // created by the data session "todo"
+  // Tip: think why we are using the "function () { ... }"
+  // in this beforeEach hook
+  cy.dataSession({
+    name: 'completedTodo',
+    setup: () => {
+      cy.request('PATCH', '/todos/' + this.todo, {
+        completed: true,
+      })
+    },
+    validate: () => {
+      return (
+        cy
+          .request('/todos/' + this.todo)
+          .its('body')
+          // the "completed" property might not even be there
+          .then((body) => Boolean(body.completed))
+      )
+    },
+    dependsOn: 'todo',
+  })
+})
+
+it('has one finished and one unfinished todo', function () {
+  // visit the page and confirm there is an item "Write more code"
+  // that has the class "completed"
+  cy.visit('/')
+  cy.contains('li.todo', 'Write more code').should(
+    'have.class',
+    'completed',
+  )
 })
