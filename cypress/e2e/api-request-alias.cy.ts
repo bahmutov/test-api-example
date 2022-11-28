ee0d2562cafd10f2dcdc4deb04e091cf8c3b3a62
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-28 08:44:56 -0500

    j1 solution

--- cypress/e2e/api-request-alias.cy.ts
+++ cypress/e2e/api-request-alias.cy.ts
@@ -1,14 +1,49 @@
-it('makes API requests using the previous value alias', () => {
-  // reset all current todo items
-  // by requesting "POST /reset" with the { todos: [] } object
-  // https://on.cypres.io/request
-  //
-  // add a new todo item by making "POST /todos"
-  // with the title "test todo"
-  //
+import 'cypress-data-session'
+
+beforeEach(() => {
+  cy.dataSession({
+    // name of the session is the alias name
+    name: 'todoId',
+    setup() {
+      // reset the existing todos
+      cy.request('POST', '/reset', { todos: [] })
+      // create the todo and yield the id
+      // that will be the aliased value
+      return cy
+        .request('POST', '/todos', {
+          title: 'test todo',
+        })
+        .its('body.id')
+    },
+    validate(todoId: string) {
+      // validate the potential id
+      // by requesting it from the server
+      // and checking the response
+      return cy
+        .request({
+          url: '/todos/' + todoId,
+          failOnStatusCode: false,
+        })
+        .then((res) => {
+          if (!res.isOkStatusCode) {
+            return false
+          }
+          return res.body.completed !== true
+        })
+    },
+  })
+})
+
+it('created the correct todo', function () {
   // request the Todo item
   // confirm the response body includes the correct title
-  //
+  cy.request('/todos/' + this.todoId)
+})
+
+it('completes the created todo', function () {
   // update the created todo item, set the "completed: true"
   // using the "PATCH /todos/:id" request
+  cy.request('PATCH', '/todos/' + this.todoId, {
+    completed: true,
+  })
 })
