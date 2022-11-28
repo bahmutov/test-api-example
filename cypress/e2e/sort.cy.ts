ef7a74d158169004ce11da606b16e81311f2f5d5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-28 09:07:16 -0500

    solution

--- cypress/e2e/sort.cy.ts
+++ cypress/e2e/sort.cy.ts
@@ -1,20 +1,18 @@
-// the initial Todo items are NOT sorted alphabetically
-const todos = [
-  {
-    id: '1',
-    title: 'ball',
-  },
-  {
-    id: '2',
-    title: 'apple',
-  },
-]
+import { map, really } from 'cypress-should-really'
+import chaiSorted from 'chai-sorted'
 
-// utility function that extracts the text contents
-// of multiple elements in the given jQuery object
-function getTexts($el: JQuery) {
-  return Cypress._.map($el, 'innerText')
-}
+chai.use(chaiSorted)
+
+// the initial random list of todo titles
+const titles = Cypress._.shuffle(
+  'several random words shuffled using lodash helper'.split(
+    ' ',
+  ),
+)
+// create the Todo items list to be sent to the server
+const todos = titles.map((title) => {
+  return { id: String(Cypress._.random(1e6)), title }
+})
 
 it('sorts items', () => {
   // reset the backend data by making an API call
@@ -25,4 +23,11 @@ it('sorts items', () => {
   cy.visit('/')
   // confirm the application shows the loaded todo items
   cy.get('li.todo').should('have.length', todos.length)
+  cy.get('[data-cy="sort-slowly"]').click()
+  // confirm the list of labels has been sorted
+  // in ascending order
+  // https://www.chaijs.com/plugins/chai-sorted/
+  cy.get('li.todo label').should(
+    really(map('innerText'), 'be.ascending'),
+  )
 })
