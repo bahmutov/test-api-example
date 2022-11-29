90ad3c552c1d28a70fbf8696a61500229e95c993
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-29 14:29:35 -0500

    viewport resolutions

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
+    env: {
+      resolutions: [
+        [1280, 1024],
+        [700, 500],
+        [300, 500],
+      ],
+    },
   },
 })
