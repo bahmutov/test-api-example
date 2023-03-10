5ef7da395c556cbb8206dd1ded8f4929b00abc78
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-10 08:24:21 -0500

    bonus 93 lesson

--- cypress/e2e/cached-or-not.cy.ts
+++ cypress/e2e/cached-or-not.cy.ts
@@ -3,21 +3,35 @@ interface Todo {
 }
 
 it('uses the data from the response if available', () => {
-  cy.intercept('/todos').as('todos')
+  // spy on the GET request to "/todos"
+  // and give the intercept an alias "todos"
+  // https://on.cypress.io/intercept
+  //
+  // visit the application
   cy.visit('/')
-  cy.wait('@todos')
-    .its('response')
-    .then((res) => {
-      expect(res?.statusCode).to.be.oneOf([200, 304])
-      if (res?.statusCode === 200) {
-        cy.log('**have response data**')
-        res?.body?.forEach((item: Todo, k: number) => {
-          cy.get('li.todo')
-            .eq(k, { log: false })
-            .contains(item.title)
-        })
-      } else {
-        cy.log('**cached data, nothing changed**')
-      }
-    })
+  // wait for the intercept "@todos"
+  // https://on.cypress.io/wait
+  // and get its response property
+  // https://on.cypress.io/its
+  //
+  // pass the response object to the cy.then(callback)
+  //
+  // check the response status code,
+  // we expect it to be either 200 or 304
+  //
+  // if the status code is 200, then the server
+  // sent the list of todos in the response body
+  // log a message to the Command Log
+  // https://on.cypress.io/log
+  //
+  // iterate over todos, and check that the application
+  // shows every todo title at its right position in the list
+  // https://on.cypress.io/get
+  // https://on.cypress.io/eq
+  // https://on.cypress.io/contains
+  //
+  // if the status code is 304, then the browser
+  // has the todos cached, and there is nothing for us to do
+  // just log a message to the Command Log
+  // https://on.cypress.io/log
 })
