bd10670228a3ba7e297c564230f64f33aad44021
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-06-27 10:13:45 -0400

    g6 lesson

--- cypress/e2e/all-aliases.cy.ts
+++ cypress/e2e/all-aliases.cy.ts
@@ -1,6 +1,5 @@
 // import commands and queries from cypress-aliases plugin
 // https://github.com/bahmutov/cypress-aliases
-import 'cypress-aliases'
 
 beforeEach(() => {
   // before each test reset everything on the server
@@ -18,11 +17,10 @@ it('has two registered aliases', () => {
   // and give the spy alias "load"
   // https://on.cypress.io/intercept
   // https://on.cypress.io/as
-  cy.intercept('GET', '/todos').as('load')
+  //
   // visit the home page
   // https://on.cypress.io/visit
-  cy.visit('/')
+  //
   // get all registered aliases
   // and confirm there are only aliases with names "three" and "load"
-  cy.getAllAliases().should('have.keys', ['three', 'load'])
 })
