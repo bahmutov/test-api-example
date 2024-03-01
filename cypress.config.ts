b7ce7dc980f312ba8a7f890b7b1b46b1ef269d2b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-01 10:24:15 -0500

    i11 solution

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,47 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
+    setupNodeEvents(on) {
+      let startedAt: number
+      let allowToFail: boolean
+
+      on('task', {
+        startProcess(canFail = false) {
+          startedAt = Date.now()
+          allowToFail = canFail
+          console.log(
+            'Long process started at %d, allowed to fail? %j',
+            startedAt,
+            allowToFail,
+          )
+          return null
+        },
+        checkProcess() {
+          if (!startedAt) {
+            throw new Error('No process started')
+          }
+
+          const now = Date.now()
+          const elapsed = now - startedAt
+          console.log(
+            'Checking process, elapsed %d ms',
+            elapsed,
+          )
+          if (elapsed > 10_000) {
+            console.log('returning success')
+            return { status: 'success' }
+          }
+          if (allowToFail) {
+            const shouldFail = Math.random() < 0.25
+            if (shouldFail) {
+              console.log('returning failing status')
+              return { status: 'failed' }
+            }
+          }
+
+          return { status: 'pending' }
+        },
+      })
+    },
   },
 })
